80cbd670fc5ebb477050fe0d866ebf81eee08682
Wojciech Zieliński <wojtekz.6776@gmail.com>
2023-05-16 17:14:09 +0200

    Create skeleton of home page

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar.jsx"
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import SpecialOffers from "./SpecialOffers.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <SpecialOffers />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import LanguageIcon from '@mui/icons-material/Language';
+
+function Navbar() {
+  return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Car Rental
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Cars
+              </a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="#">
+                Help
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+				<LanguageIcon />
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                  English
+                </a>
+                <a class="dropdown-item" href="#">
+                  Polish
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  );
+}
+
+export default Navbar;
